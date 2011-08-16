ca23b7db8f2267b6b6273a9094fab25f6e251265
aoanla@gmail.com <aoanla@users.noreply.github.com>
2011-08-16 21:00:52 +0000

    Make splitvideotracks generic to n video tracks in a movie, make more pythonic

--- splitvideotracks.py
+++ splitvideotracks.py
@@ -6,35 +6,34 @@ import sys
 #get args
 filename = sys.argv[1] #or whatever
 
+#m_in is our source movie
 m_in = QTKit.QTMovie.movieWithFile_(filename)
 e = None
-m_out1 = QTKit.QTMovie.movie()
-m_out2 = QTKit.QTMovie.movie()
 
+#need number of video tracks
+video_tracks = m_in.tracksOfMediaType_(QTKit.QTMediaTypeVideo)
+
+#m_outs are our destinations, zip them with their track here
+m_out = [(QTKit.QTMovie.movie(),v) for v in video_tracks]
+
+#need the time points to copy to the new movies - can't just add tracks due to QTKit limitations
 dura = m_in.duration()
 start = m_in.currentTime()
 t_range = QTKit.QTMakeTimeRange(start,dura)
 
-m_out1.setAttribute_forKey_(True, QTKit.QTMovieEditableAttribute)
-m_out2.setAttribute_forKey_(True, QTKit.QTMovieEditableAttribute)
-
-#first add sound
+#get soundtrack ahead of time, as it's the same for all of them
 soundtrack = m_in.tracksOfMediaType_(QTKit.QTMediaTypeSound)[0]
 
-m_out1.insertSegmentOfTrack_timeRange_atTime_(soundtrack, t_range, start)
-m_out2.insertSegmentOfTrack_timeRange_atTime_(soundtrack, t_range, start)
-
-#then add split video
-video_tracks = m_in.tracksOfMediaType_(QTKit.QTMediaTypeVideo)
-
-m_out1.insertSegmentOfTrack_timeRange_atTime_(video_tracks[0], t_range, start)
-m_out2.insertSegmentOfTrack_timeRange_atTime_(video_tracks[1], t_range, start)
-
-#then write out to files
+#Define mode to write out to files
 d = Foundation.NSMutableDictionary.dictionary()
 d['QTMovieFlatten'] = True
 
-m_out1.writeToFile_withAttributes_(filename + '0.mov', d)
-m_out2.writeToFile_withAttributes_(filename + '1.mov', d)
-
+#iterate through movies, making writable (QTKit oddness), adding soundtrack, videotrack, write out to disk
+index = 0
+for m in m_out:
+	m[0].setAttribute_forKey_(True, QTKit.QTMovieEditableAttribute)
+	m[0].insertSegmentOfTrack_timeRange_atTime_(soundtrack, t_range, start)
+	m[0].insertSegmentOfTrack_timeRange_atTime_(m[1], t_range, start)
+	m[0].writeToFile_withAttributes_(filename + str(index), d)
+	index = index + 1
 #done!
